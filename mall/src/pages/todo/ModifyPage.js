134edebe5f4168b78f34d6598514dd5273ac5ad4
java_teacher76@daum.net <java_teacher76@daum.net>
2024-07-27 17:16:29 +0900

    modified:   mall/src/pages/todo/ModifyPage.js

--- mall/src/pages/todo/ModifyPage.js
+++ mall/src/pages/todo/ModifyPage.js
@@ -5,12 +5,6 @@ import ModifyComponent from "../../components/todo/ModifyComponent";
 const ModifyPage = () => {
     const {tno} = useParams()
     const navigate = useNavigate()
-    const moveToRead = () => {
-        navigate({pathname:`/todo/read/${tno}`})
-    }
-    const moveToList = () => {
-        navigate({pathname:`/todo/list`})
-    }
     return (
         <div className="p-4 w-full bg-white">
             <div className="text=3xl font-extrabold">
